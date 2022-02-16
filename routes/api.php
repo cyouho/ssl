e1e7c3274c942c84a2d959ee7b7cabaaf74fe774
zhangfan <cyouho@hotmail.co.jp>
2022-02-16 11:00:00 +0800

    添加测试用migration，用户账户model，测试用api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    return 'test';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
