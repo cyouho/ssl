19e1242c0ff10f413f52e9f2d8aa8a10777e4404
zhangfan <cyouho@hotmail.co.jp>
2022-02-19 09:24:09 +0800

    创建controller的工具类，完善迁移文件，添加用户注册方法

--- app/Models/UserAccount.php
+++ app/Models/UserAccount.php
@@ -12,4 +12,24 @@ use Illuminate\Database\Eloquent\Model;
 class UserAccount extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     * 数据库列白名单
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * Insert user data
+     * 插入用户数据
+     * 
+     * 
+     */
+    public function insertUserData($data)
+    {
+    }
 }
