4ec40688de469aa194aeeb679464867bd1c91d3d
zhangfan <cyouho@hotmail.co.jp>
2022-02-20 09:24:54 +0800

    添加服务、用户服务前移文件以及model文件，完善认证controller、用户账户controller

--- app/Models/UserAccount.php
+++ app/Models/UserAccount.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Model for User Account
@@ -27,9 +28,14 @@ class UserAccount extends Model
      * Insert user data
      * 插入用户数据
      * 
+     * @param array $data <User data | 用户数据>
      * 
+     * @return int $id <Inserted user id | 插入的用户id>
      */
     public function insertUserData($data)
     {
+        $id = DB::table('user_accounts')->insertGetId($data);
+
+        return $id;
     }
 }
